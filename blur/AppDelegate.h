b416a5664249cdd2fe2328dae36dd866b7788889
abhiii <HadiHatinoglu@Hadis-iMac.local>
2013-11-20 11:36:08 +0530

    Initial Commit

---
+++ blur/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  blur
+//
+//  Created by Hadi Hatinoglu on 20/11/13.
+//  Copyright (c) 2013 Baraansoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ blur/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  blur
+//
+//  Created by Hadi Hatinoglu on 20/11/13.
+//  Copyright (c) 2013 Baraansoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
